a92fe1078a112c3bf25c8d2b611c861c4d2d6f0d
IrinaPavlova <ispavlovais@gmail.com>
2016-04-25 22:32:29 +0300

    Counting mediana

--- length_of_paragraph.py
+++ length_of_paragraph.py
@@ -1,7 +1,9 @@
 import os
 import re
+import numpy as np
 
 for root, dirs, files in os.walk('./Corpus/Suspense'):
+    all = []
     lens = {}
     for f in files:
         if f.endswith('.txt'):
@@ -15,15 +17,18 @@ for root, dirs, files in os.walk('./Corpus/Suspense'):
                     sentences.append(i)
             # print(sentences)
             # print(len(sentences))
+            all.append(len(sentences))
             if len(sentences) in lens:
                 lens[len(sentences)] += 1
             else:
                 lens[len(sentences)] = 1
-    print('SUSPENSE DATA')
-    for i in lens:
-        print(str(i) + ' : ' + str(round((lens[i]*100)/136, 2)))
+    print(np.median(all))
+    #print('SUSPENSE DATA')
+    #for i in lens:
+        #print(str(i) + ' : ' + str(round((lens[i]*100)/136, 2)))
 
 for root, dirs, files in os.walk('./Corpus/Unsuspense'):
+    all = []
     lens = {}
     for f in files:
         if f.endswith('.txt'):
@@ -35,13 +40,15 @@ for root, dirs, files in os.walk('./Corpus/Unsuspense'):
             for i in o:
                 if i is not '':
                     sentences.append(i)
-            # print(sentences)
-            # print(len(sentences))
+            #print(sentences)
+            #print(len(sentences))
+            all.append(len(sentences))
             if len(sentences) in lens:
                 lens[len(sentences)] += 1
             else:
                 lens[len(sentences)] = 1
-    print('UNSUSPENSE DATA')
-    for i in lens:
-        print(str(i) + ' : ' + str(round((lens[i]*100)/28, 2)))
+    print(np.median(all))
+    #print('UNSUSPENSE DATA')
+    #for i in lens:
+        #print(str(i) + ' : ' + str(round((lens[i]*100)/28, 2)))
 
