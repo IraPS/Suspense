4fd131969a8ee6b7946056a146db713951045398
IrinaPavlova <ispavlovais@gmail.com>
2016-05-11 19:46:10 +0300

    improvements

--- regression.py
+++ regression.py
@@ -2,8 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.metrics import roc_curve, auc
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics
+from sklearn.cross_validation import cross_val_score
 
-train = np.load('train.npz')
-train = train['arr_0']
-test = np.load('test.npz')
-test = test['arr_0']
+X = np.load('all_features.npz')['arr_0']
+y = np.array([0]*28 + [1]*136)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+
+predicted = model.predict(X_test)
+
+for i in range(len(y_test)):
+    print(predicted[i], y_test[i])
+
+scores = cross_val_score(LogisticRegression(), X, y, scoring='accuracy', cv=20)
+print(scores)
+print(scores.mean())
