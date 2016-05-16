f62439fc8b98d7ece1ef435a39cd3a5e7dad63fc
IrinaPavlova <ispavlovais@gmail.com>
2016-05-16 14:32:45 +0300

    improvements

--- net_try.py
+++ net_try.py
@@ -8,35 +8,38 @@ from sklearn.cross_validation import train_test_split
 from sklearn import metrics
 from sklearn.cross_validation import cross_val_score
 from sknn.mlp import Classifier, Layer
+from sklearn.metrics import classification_report
 
 
 def run():
+
     nn = Classifier(
         layers=[
-            Layer("Sigmoid", units=10),
+            Layer("ExpLin", units=5),
             Layer("Softmax")],
-        learning_rate=0.02,
+        learning_rate=0.0002,
         learning_rule='nesterov',
-        n_iter=20)
+        n_iter=50, valid_size=0.1)
 
     X = np.load('All_features.npz')['arr_0']
+
     all_samples = [1]*141 + [0]*123
     y = np.array(all_samples)
 
-    X_train, X_t, y_train, y_t = train_test_split(X, y, test_size=0.5, random_state=0)
-    X_test, X_valid, y_test, y_valid = train_test_split(X_t, y_t, test_size=0.5, random_state=0)
-
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=0)
 
     nn.fit(X_train, y_train)
 
-    y_valid = nn.predict(X_valid)
+    #y_valid = nn.predict(X_valid)
 
     score = nn.score(X_test, y_test)
 
-    print('Accuracy is', score)
+    return score
 
+#print(run())
 
-for i in range(10): run()
+for i in range(10): print(run())
 
-# средняя точность ~ 0,6388
+# Sigmoid средняя точность ~ 0,6388 (сильный разброс - 20 :(()
+# ExpLin средняя точность ~ 0,667 (не такой сильный разброс - 10 :( )
 
