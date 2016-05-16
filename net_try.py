a1f5480adf692908e1323839dee0e262bd6b0a5b
IrinaPavlova <ispavlovais@gmail.com>
2016-05-16 13:41:00 +0300

    improvements

--- net_try.py
+++ net_try.py
@@ -9,24 +9,34 @@ from sklearn import metrics
 from sklearn.cross_validation import cross_val_score
 from sknn.mlp import Classifier, Layer
 
-nn = Classifier(
-    layers=[
-        Layer("Rectifier", units=100),
-        Layer("Softmax")],
-    learning_rate=0.02,
-    n_iter=10)
 
-X = np.load('All_features.npz')['arr_0']
-all_samples = [1]*141 + [0]*123
-y = np.array(all_samples)
+def run():
+    nn = Classifier(
+        layers=[
+            Layer("Sigmoid", units=10),
+            Layer("Softmax")],
+        learning_rate=0.02,
+        learning_rule='nesterov',
+        n_iter=20)
 
-X_train, X_t, y_train, y_t = train_test_split(X, y, test_size=0.5, random_state=0)
-X_test, X_valid, y_test, y_valid = train_test_split(X_t, y_t, test_size=0.5, random_state=0)
+    X = np.load('All_features.npz')['arr_0']
+    all_samples = [1]*141 + [0]*123
+    y = np.array(all_samples)
+
+    X_train, X_t, y_train, y_t = train_test_split(X, y, test_size=0.5, random_state=0)
+    X_test, X_valid, y_test, y_valid = train_test_split(X_t, y_t, test_size=0.5, random_state=0)
 
 
-nn.fit(X_train, y_train)
+    nn.fit(X_train, y_train)
 
-y_valid = nn.predict(X_valid)
+    y_valid = nn.predict(X_valid)
 
-score = nn.score(X_test, y_test)
+    score = nn.score(X_test, y_test)
+
+    print('Accuracy is', score)
+
+
+for i in range(10): run()
+
+# средняя точность ~ 0,6388
 
