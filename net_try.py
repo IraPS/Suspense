4ff2661292ace01366fef96a48c43a8269d0c035
IrinaPavlova <ispavlovais@gmail.com>
2016-05-16 19:00:00 +0300

    improvements

--- net_try.py
+++ net_try.py
@@ -9,6 +9,9 @@ from sklearn import metrics
 from sklearn.cross_validation import cross_val_score
 from sknn.mlp import Classifier, Layer
 from sklearn.metrics import classification_report
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
 
 
 def run():
@@ -21,18 +24,24 @@ def run():
         learning_rule='nesterov',
         n_iter=50, valid_size=0.1)
 
+    pipeline = Pipeline([
+        ('min/max scaler', MinMaxScaler(feature_range=(0.0, 1.0))),
+        ('neural network', Classifier(layers=[Layer("Softmax")], n_iter=25))])
+
+
+
     X = np.load('All_features.npz')['arr_0']
 
     all_samples = [1]*141 + [0]*123
     y = np.array(all_samples)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    nn.fit(X_train, y_train)
+    pipeline.fit(X_train, y_train)
+    score = pipeline.score(X_train, y_train)
 
-    #y_valid = nn.predict(X_valid)
-
-    score = nn.score(X_test, y_test)
+    # nn.fit(X_train, y_train)
+    # score = nn.score(X_train, y_train)
 
     return score
 
