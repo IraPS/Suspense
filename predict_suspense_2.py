031362210e15ddc86ae56012304ca6314b39de57
IrinaPavlova <ispavlovais@gmail.com>
2016-06-01 17:13:50 +0300

    improvements

---
+++ predict_suspense_2.py
@@ -0,0 +1,26 @@
+from all_features import getFeatures
+import numpy as np
+import pickle
+
+novel = 'try'
+features = getFeatures(novel)
+np.savez_compressed(novel + '_features.npz', features)
+
+nn = pickle.load(open('NeuralNet_model.pkl', 'rb'))
+
+D = np.load(novel + '_features.npz')['arr_0']
+
+prediction = nn.predict(D)
+probs = nn.predict_proba(D)
+
+simple_predicts = []
+scale_predicts = []
+for i in prediction:
+    simple_predicts.append(i[0])
+for i in probs:
+    scale_predicts.append(i[1]*10)
+    compare = []
+
+print('Simple prediction (suspenseful / unsuspensful) paragraph:\n', simple_predicts, '\n\n')
+
+print('Scale prediction for each paragraph:\n', scale_predicts)
