4701944c5504a26ffd9549620ab5c85433fc20e2
IrinaPavlova <ispavlovais@gmail.com>
2016-06-01 17:14:16 +0300

    improvements

---
+++ predict_suspense_1.py
@@ -0,0 +1,15 @@
+import re
+from break_text_into_paragraphs import break_into_paragraphs
+from prepare_corpus import prepare
+
+novel = input('Please type the name of the novel which stored in a .txt file: ')
+
+file = open(novel + '.txt', 'r', encoding='utf-8')
+text = file.read()
+text = re.sub(':\n-', ':\n -', text)
+text = re.sub(':\n–', ':\n –', text)
+text = text.split('\n')
+
+break_into_paragraphs(text, novel)
+
+prepare('try')
